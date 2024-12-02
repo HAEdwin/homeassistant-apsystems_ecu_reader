7a8fbd6876cff544c0ae756bd3d7796bae5b53aa
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2024-12-02 20:02:12 +0100

    Create const.py

---
+++ const.py
@@ -0,0 +1,20 @@
+DOMAIN = 'apsystems_ecur'
+
+SOLAR_ICON = "mdi:solar-power"
+FREQ_ICON = "mdi:sine-wave"
+SIGNAL_ICON = "mdi:signal"
+RELOAD_ICON = "mdi:reload"
+CACHE_ICON = "mdi:cached"
+RESTART_ICON = "mdi:restart"
+POWER_ICON = "mdi:power"
+
+
+# Config flow schema. These are also translated through associated json translations
+KEYS = [
+    "ecu_host",
+    "scan_interval",
+    "port_retries",
+    "wifi_ssid",
+    "wifi_password",
+    "show_graphs",
+]
