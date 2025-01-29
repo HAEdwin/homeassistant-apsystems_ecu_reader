23aa9665c72d5d963c3d2634f0a696b0aa0f1143
HAEdwin <82239374+HAEdwin@users.noreply.github.com>
2025-01-29 07:59:18 +0100

    Update switch.py

--- custom_components/apsystems_ecu_reader/switch.py
+++ custom_components/apsystems_ecu_reader/switch.py
@@ -1,3 +1,5 @@
+"""Support for APsystems ECU switches."""
+
 import logging
 
 from homeassistant.components.switch import SwitchEntity
@@ -8,91 +10,26 @@ from homeassistant.helpers.update_coordinator import (
 
 from .const import (
     DOMAIN,
-    RELOAD_ICON,
     POWER_ICON,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, config, add_entities, discovery_info=None):
-    """Set up the APsystems ECU switches."""	
+async def async_setup_entry(hass, _, add_entities):
+    """Set up the APsystems Inverter switches."""
 
     ecu = hass.data[DOMAIN].get("ecu")
     coordinator = hass.data[DOMAIN].get("coordinator")
-    switches = [
-        APsystemsECUQuerySwitch(coordinator, ecu, "query_device",
-            label="Query Device", icon=RELOAD_ICON),
-    ]
+    switches = []
     inverters = coordinator.data.get("inverters", {})
     for uid, inv_data in inverters.items():
         switches.append(APsystemsECUInverterSwitch(coordinator, ecu, uid, inv_data))
     add_entities(switches)
 
-class APsystemsECUQuerySwitch(CoordinatorEntity, SwitchEntity):
-    """Representation of a switch."""	
-    def __init__(self, coordinator, ecu, field, label=None, icon=None):
-        super().__init__(coordinator)
-        self.coordinator = coordinator
-        self._ecu = ecu
-        self._field = field
-        self._label = label
-        if not label:
-            self._label = field
-        self._icon = icon
-        self._name = f"ECU {self._ecu.ecu.ecu_id} {self._label}"
-        self._state = True
-
-    @property
-    def unique_id(self):
-        """Return the unique id of the switch."""	
-        return f"{self._ecu.ecu.ecu_id}_{self._field}"
-
-    @property
-    def name(self):
-        """Return the name of the switch."""
-        return self._name
-
-    @property
-    def icon(self):
-        """Return the icon to use in the frontend, if any."""	
-        return self._icon
-
-    @property
-    def device_info(self):
-        """Return the device info."""	
-        parent = f"ecu_{self._ecu.ecu.ecu_id}"
-        return {
-            "identifiers": {
-                (DOMAIN, parent),
-            }
-        }
-
-    @property
-    def entity_category(self):
-        """Return the category of the entity."""	
-        return EntityCategory.CONFIG
-
-    @property
-    def is_on(self):
-        """Return the state of the switch."""	
-        return self._ecu.is_querying
-
-    def turn_off(self, *args, **kwargs):
-        """Turn off the switch."""
-        self._ecu.set_querying_state(False)
-        self._state = False
-        self.schedule_update_ha_state()
-
-    def turn_on(self, *args, **kwargs):
-        """Turn on the switch."""
-        self._ecu.set_querying_state(True)
-        self._state = True
-        self.schedule_update_ha_state()
-
 
 class APsystemsECUInverterSwitch(CoordinatorEntity, SwitchEntity):
     """Representation of a switch for an individual inverter."""
-    def __init__(self, coordinator, ecu, uid, inv_data, icon=None):
+    def __init__(self, coordinator, ecu, uid, inv_data):
         super().__init__(coordinator)
         self.coordinator = coordinator
         self._ecu = ecu
@@ -100,7 +37,6 @@ class APsystemsECUInverterSwitch(CoordinatorEntity, SwitchEntity):
         self._inv_data = inv_data
         self._name = f"Inverter {uid} On/Off"
         self._state = True
-
 
     @property
     def unique_id(self):
@@ -114,17 +50,21 @@ class APsystemsECUInverterSwitch(CoordinatorEntity, SwitchEntity):
 
     @property
     def icon(self):
-        """Return the icon to use in the frontend"""
+        """Return the icon to use in the UI."""
         return POWER_ICON
 
     @property
     def device_info(self):
         """Return the device info."""
-        parent = f"ecu_{self._ecu.ecu.ecu_id}"
+        parent = f"inverter_{self._uid}"
         return {
             "identifiers": {
                 (DOMAIN, parent),
-            }
+            },
+            "name": f"Inverter {self._uid}",
+            "manufacturer": "APsystems",
+            "model": self._inv_data.get("model", "Unknown"),
+            "via_device": (DOMAIN, f"ecu_{self._ecu.ecu.ecu_id}"),
         }
 
     @property
@@ -134,17 +74,17 @@ class APsystemsECUInverterSwitch(CoordinatorEntity, SwitchEntity):
 
     @property
     def is_on(self):
-        """ Return the state of the switch """
+        """Return the state of the inverter switch."""
         return self._state
 
     def turn_off(self, *args, **kwargs):
-        """Turn off the switch."""
+        """Turn off the inverter switch."""
         self._ecu.set_inverter_state(self._uid, False)
         self._state = False
         self.schedule_update_ha_state()
 
     def turn_on(self, *args, **kwargs):
-        """Turn on the switch."""
+        """Turn on the inverter switch."""
         self._ecu.set_inverter_state(self._uid, True)
         self._state = True
         self.schedule_update_ha_state()
